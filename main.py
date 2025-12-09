6b9f60286a7f4fef35ddc6c03d1dd8325f56689d
Lilyinit <iamlilyinit@gmail.com>
2025-12-08 21:03:16 -0500

    fix: fix import errors

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel 
 
 # LangChain/OpenAI related imports
-from langchain.schema import HumanMessage
-
-# 수정된 코드 (ChatOpenAI를 langchain_openai에서 가져옵니다)
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI 
-from langchain.schema import HumanMessage 
+
 
 # ... (나머지 코드는 동일)
 
